7eeae5e5bd28c8164a13b25c97ce7f7c5e6842c6
lewis.george.norris@gmail.com <lewis.george.norris@gmail.com>
2021-01-15 12:40:20 +0000

    Added methods - new transaction and get last block

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,22 +1,53 @@
 function Blockchain() {
   this.chain = [];
-  this.newTransactions = [];
+  this.pendingTransactions = [];
 }
 
-Blockchain.prototype.createNewBlock = (nonce, previousBlockHash, hash) => {
+/**
+ * Creates new block and adds to blockchain
+ * @param {number} nonce - number which represents a proof of work using a p.o.w method
+ * @param {string} previousBlockHash - unique hash of previous block on blockchain
+ * @param {string} hash - unique hash of this block
+ */
+
+Blockchain.prototype.createNewBlock = function (nonce, previousBlockHash, hash) {
   const newBlock = {
     index: this.chain.length + 1,
     timestamp: Date.now(),
-    transactions: this.newTransactions,
+    transactions: this.pendingTransactions,
     nonce: nonce,
     hash: hash,
     previousBlockHash: previousBlockHash
   };
-  
-  this.newTransactions = [];
+  this.pendingTransactions = [];
   this.chain.push(newBlock)
-
   return newBlock;
+}
+
+/**
+ * Return last block on blockchain
+ * @returns {object} - returns last block object on block chain
+ */
+
+Blockchain.prototype.getLastBlock = function ()  {
+  return this.chain[this.chain.length - 1]
+}
+
+/**
+ * Create new transaction prior to next block creation
+ * @param {number} amount - value of amount sent in transaction
+ * @param {string} sender - address of person who is initiating transaction and sending the above amount
+ * @param {string} recipient  - address of person who is receiving the transaction
+ */
+
+Blockchain.prototype.createNewTransaction = function(amount, sender, recipient) {
+  const newTransaction = {
+    amount: amount,
+    sender: sender,
+    recipient: recipient
+  }
+  this.pendingTransactions.push(newTransaction)
+  return this.getLastBlock()['index'] + 1
 }
 
 module.exports = Blockchain;
