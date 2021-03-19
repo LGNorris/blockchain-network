15ea3a027c0e705ff23dfda46c6f3ba3f1f5cc54
lewis.george.norris@gmail.com <lewis.george.norris@gmail.com>
2021-03-19 11:21:06 +0000

    Added detach run docker command

---
+++ docker-build-detach.sh
@@ -0,0 +1,9 @@
+ #!/bin/bash
+
+ # exports env vars from local env file for access
+export $(grep -v '#.*' .env | xargs)
+
+# pass exported env vars as build args to docker build. Args are then passed the env vars in docker file.
+docker build --build-arg PORT=$PORT --build-arg CORS_ORIGIN=$CORS_ORIGIN --build-arg IP=$IP -t blockchain-network-v01 .    
+
+docker run -td -p 8080:8080 blockchain-network-v01 
