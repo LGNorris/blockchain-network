6d98484c40a88ad911235bcbe1d6e6bcc15c466d
Lewis Norris <lewis.george.norris@gmail.com>
2021-07-16 09:38:12 +0100

    Added node port

--- docker-build.sh
+++ docker-build.sh
@@ -4,6 +4,6 @@
 export $(grep -v '#.*' .env | xargs)
 
 # pass exported env vars as build args to docker build. Args are then passed the env vars in docker file.
-docker build --build-arg PORT=$PORT --build-arg CORS_ORIGIN=$CORS_ORIGIN --build-arg IP=$IP -t blockchain-network-v01 .    
+docker build --build-arg PORT=$PORT --build-arg CORS_ORIGIN=$CORS_ORIGIN --build-arg IP=$IP --build-arg NODE_PORT=$NODE_PORT -t blockchain-network-v01 .    
 
 docker run -p 8080:8080 blockchain-network-v01 
